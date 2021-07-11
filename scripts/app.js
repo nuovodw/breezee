8764fe016d43a68a62116eee58f3f60c38766051
Daniel Won <cantabile3@hotmail.com>
2021-07-11 09:29:53 -0500

    edit script

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,15 @@
+const currentCity = document.querySelector('.city');
+const currentTemp = document.querySelector('.temp');
+const weatherText = document.querySelector('.weather-text');
+const wind = document.querySelector('.wind-speed');
+const uv = document.querySelector('.uv');
+const humidity = document.querySelector('.humidity');
+
+const feelsLike = document.querySelector('.feels-like');
+
 let weatherToday = {
 	api: 'p6WqcUxVySBDjHDgfTYjzEwixHbo0GAx',
+
 	getCity: function (city) {
 		fetch(
 			`https://dataservice.accuweather.com/locations/v1/search?q=${city}&apikey=${this.api}`,
@@ -7,19 +17,29 @@ let weatherToday = {
 			.then((response) => response.json())
 			.then((data) => this.getWeather(data));
 	},
+
 	getWeather: function (data) {
 		const cityKey = data[0].Key;
+		const cityName = data[0].EnglishName;
 		fetch(
-			`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${this.api}&details=true`,
+			`https://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${this.api}&details=true`,
 		)
 			.then((response) => response.json())
 			.then((data) => this.displayWeather(data));
+		currentCity.innerText = `${data[0].EnglishName}`;
 	},
+
 	displayWeather: function (data) {
-		console.log(data[0].WeatherText);
-		const { WeatherText } = data[0].WeatherText;
-		console.log(WeatherText);
+		const newData = data[0];
+		currentTemp.innerText = newData.Temperature.Imperial.Value + '°';
+		weatherText.innerText = newData.WeatherText;
+		feelsLike.innerText = newData.RealFeelTemperature.Imperial.Value + '°';
+		wind.innerText = newData.WindGust.Speed.Imperial.Value + 'mi/h';
+		uv.innerText = 'UV: ' + newData.UVIndex;
+		humidity.innerText = newData.RelativeHumidity + '%';
 	},
 };
 
-weatherToday.getCity('Chicago');
+weatherToday.getCity('chicago');
+
+// to do: hi/lo, weather icons; sunrise/set, change of rain
