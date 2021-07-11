323b3bbf1f3c65642fecc32053601591545d1e93
Daniel Won <cantabile3@hotmail.com>
2021-07-11 08:04:25 -0500

    edit script

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,25 @@
+let weatherToday = {
+	api: 'p6WqcUxVySBDjHDgfTYjzEwixHbo0GAx',
+	getCity: function (city) {
+		fetch(
+			`https://dataservice.accuweather.com/locations/v1/search?q=${city}&apikey=${this.api}`,
+		)
+			.then((response) => response.json())
+			.then((data) => this.getWeather(data));
+	},
+	getWeather: function (data) {
+		const cityKey = data[0].Key;
+		fetch(
+			`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${this.api}&details=true`,
+		)
+			.then((response) => response.json())
+			.then((data) => this.displayWeather(data));
+	},
+	displayWeather: function (data) {
+		console.log(data[0].WeatherText);
+		const { WeatherText } = data[0].WeatherText;
+		console.log(WeatherText);
+	},
+};
 
+weatherToday.getCity('Chicago');
